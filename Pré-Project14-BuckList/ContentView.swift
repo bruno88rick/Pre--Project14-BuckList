9dc6e2afae95696c667f6ceb5e4b01b895e66915
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-09-24 15:48:04 -0300

    Initial Commit

---
+++ Pré-Project14-BuckList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pré-Project14-BuckList
+//
+//  Created by Bruno Oliveira on 24/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pré-Project14-BuckList/Pre__Project14_BuckListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pre__Project14_BuckListApp.swift
+//  Pré-Project14-BuckList
+//
+//  Created by Bruno Oliveira on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Pre__Project14_BuckListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
